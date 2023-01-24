c74b8fe775dea87acd104fa39794020061422a30
Srdjan Krsmanovic <srdjan.krsmanovic@prodyna.comn@gmail.com>
2023-01-24 23:10:30 +0100

    Adding Eureka microservice

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spring-cloud-example
 Spring cloud microservice repo example
+
+# Getting Started
+To start Eureka server, run the following command in eureka folder:
+
+```
+./gradlew bootRun
+```
+Then, goto http://localhost:8761 to see the Eureka dashboard.
+
---
+++ eureka/src/test/java/com/sk/springcloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sk.springcloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
