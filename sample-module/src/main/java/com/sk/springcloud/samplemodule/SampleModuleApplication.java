c009d8741b9300c35b1de0aea07781dbaa3ac64b
Srdjan Krsmanovic <krsrdjan@gmail.com>
2023-01-26 00:16:41 +0100

    Adding sample module with Spring boot 2.7.8 deps

---
+++ sample-module/src/main/java/com/sk/springcloud/samplemodule/SampleModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sk.springcloud.samplemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleModuleApplication.class, args);
+	}
+
+}
---
+++ sample-module/src/test/java/com/sk/springcloud/samplemodule/SampleModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.springcloud.samplemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
